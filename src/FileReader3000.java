800a1f51f0abbafb2bba5749a437d3cfcaba31f3
David <david.saltorp@gmail.com>
2023-02-19 20:36:14 +0100

    2nd

--- src/FileReader3000.java
+++ src/FileReader3000.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.io.File;
+import java.util.Scanner;
+
+public class FileReader3000 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        File file = new File("/Brugere/catharinasaltorp-jorgensen/Overførsler/captmidn.txt");
+        Scanner scan = new Scanner(file);
+
+        while(scan.hasNextLine()) {
+
+            System.out.println(scan.nextLine());
+
+        }
     }
 }
